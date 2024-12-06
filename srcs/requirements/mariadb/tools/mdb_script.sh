f2e165f39742cb9cedc82cccea0fd2dc621d4f22
fmoran-m <franmoranmato@gmail.com>
2024-12-06 22:05:52 +0100

    mariadb almost done

--- srcs/requirements/mariadb/tools/mdb_script.sh
+++ srcs/requirements/mariadb/tools/mdb_script.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-mysql -u root -e "ALTER USER 'root'@'%' IDENTIFIED BY '${MDB_ROOT_PASS}';"
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS '${MDB_DB}';"
-mysql -u root -e "CREATE USER IF NOT EXISTS '${MDB_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON '${MDB_DB}'.* TO '${MDB_USER}'@'%';"
+mysqld --datadir=/var/lib/mysql --user=root &
+
+MYSQL_PID=$!
+
+until mysqladmin ping --silent; do
+    sleep 2
+done
+
+mysql -u root -e "CREATE DATABASE ${MDB_DB}"
+mysql -u root -e "CREATE USER '${MDB_USER}'@'%' IDENTIFIED BY '${MDB_USER_PASS}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON ${MDB_DB}.* TO '${MDB_USER}'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
-mysqld --datadir=/var/lib/mysql --user=root
+kill $MYSQL_PID
+
+exec mysqld --datadir=/var/lib/mysql --user=root
