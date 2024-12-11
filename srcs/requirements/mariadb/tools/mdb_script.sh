2369ea0fc0fd25e830adb8299e03f1e5ed2bdee1
fmoran-m <franmoranmato@gmail.com>
2024-12-11 19:57:32 +0100

    connect to database

--- srcs/requirements/mariadb/tools/mdb_script.sh
+++ srcs/requirements/mariadb/tools/mdb_script.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+rm -f /etc/my.cnf
 touch /tmp/init_file && chmod 777 /tmp/init_file
 
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MDB_ROOT_PASS}';"
-echo "CREATE DATABASE ${MDB_DB};" >> /tmp/init_file
-echo "CREATE USER '${MDB_USER}'@'%' IDENTIFIED BY '${MDB_USER_PASS}';" >> /tmp/init_file
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MDB_ROOT_PASS}';" >> /tmp/init_file
+echo "CREATE DATABASE IF NOT EXISTS ${MDB_DB};" >> /tmp/init_file
+echo "CREATE USER IF NOT EXISTS '${MDB_USER}'@'%' IDENTIFIED BY '${MDB_USER_PASS}';" >> /tmp/init_file
 echo "GRANT ALL PRIVILEGES ON ${MDB_DB}.* TO '${MDB_USER}'@'%';" >> /tmp/init_file
 echo "FLUSH PRIVILEGES;" >> /tmp/init_file
 
