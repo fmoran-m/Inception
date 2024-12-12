49932634ab9e3e0f88418c62ecebfe99ab96ce33
fmoran-m <franmoranmato@gmail.com>
2024-12-12 21:40:56 +0100

    works?

--- srcs/requirements/wordpress/tools/www_script.sh
+++ srcs/requirements/wordpress/tools/www_script.sh
@@ -2,11 +2,17 @@
 
 FILE=/var/www/html/wp-config.php
 
-if [ -f /var/www/html/wp-config-sample.php ]
+if [ -f /var/www/html/wp-config-sample.php ]; then
     mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+fi
 
-sed /define( 'DB_USER', 'username_here' );/define( 'DB_USER', '${MDB_USER}' );/g
-sed /define( 'DB_PASSWORD', 'password_here' );/define( 'DB_PASSWORD', '${MDB_USER_PASS}' );/g
-sed /define( 'DB_HOST', 'localhost' );/define( 'DB_USER', 'mariadb:3306' );/g
+sed -i "s/define( 'DB_NAME', 'database_name_here' );/define( 'DB_NAME', '${MDB_DB}' );/g" $FILE
+sed -i "s/define( 'DB_USER', 'username_here' );/define( 'DB_USER', '${MDB_USER}' );/g" $FILE
+sed -i "s/define( 'DB_PASSWORD', 'password_here' );/define( 'DB_PASSWORD', '${MDB_USER_PASS}' );/g" $FILE
+sed -i "s/define( 'DB_HOST', 'localhost' );/define( 'DB_HOST', 'mariadb:3306' );/g" $FILE
+
+cd /var/www/html
+#wp core config --dbname="$MDB_DB" --dbuser="$MDB_USER" --dbpass="$MDB_USER_PASS" --dbhost=mariadb:3306
+wp core install --url=https://fmoran-m.42.fr --title=Inception --admin_user="$WP_USER" --admin_password="$WP_PASS" --admin_email=false_mail@gmail.com --skip-email
 
 /usr/sbin/php-fpm82 -F
